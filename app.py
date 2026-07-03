b4d2152cc79d807400b34585d8a2b565739dbdb2
Divya Satya <divyasatya650@gmail.com>
2026-07-03 13:18:22 +0530

    Build TuneTrack application entry point

---
+++ app.py
@@ -0,0 +1,7 @@
+
+from db import connection, cursor
+
+print("========================================")
+print("🎵 Welcome to TuneTrack")
+print("Decode Your Musical DNA")
+print("========================================")
